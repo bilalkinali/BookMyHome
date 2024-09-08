6f80d3f5941938d3e6f774f049abdaad0d335ef7
Bilal Kinali <biki59986@edu.ucl.dk>
2024-09-08 12:52:14 +0200

    Added UnitOfWork class+interface with serializable transaction isolation level

---
+++ BookMyHome.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using BookMyHome.Application.Helpers;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace BookMyHome.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _db;
+        private IDbContextTransaction? _transaction;
+
+        public UnitOfWork(DbContext db)
+        {
+            _db = db;
+        }
+
+        void IUnitOfWork.BeginTransaction(IsolationLevel isolationLevel)
+        {
+            if (_db.Database.CurrentTransaction != null) return;
+            _transaction = _db.Database.BeginTransaction(isolationLevel);
+        }
+
+        void IUnitOfWork.Commit()
+        {
+            if (_transaction == null) throw new Exception("You must call 'Begin Transaction' before Commit() is called");
+            _transaction.Commit();
+            _transaction.Dispose();
+        }
+
+        void IUnitOfWork.Rollback()
+        {
+            if (_transaction == null) throw new Exception("You must call 'Begin Transaction' before Rollback() is called");
+            _transaction.Rollback();
+            _transaction.Dispose();
+        }
+    }
+}
