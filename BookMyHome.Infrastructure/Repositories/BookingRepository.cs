3d09c3f84e72c71f993f1ff29d40569043ca7287
Bilal Kinali <biki59986@edu.ucl.dk>
2024-09-18 01:16:23 +0200

    cleanup

--- BookMyHome.Infrastructure/Repositories/BookingRepository.cs
+++ BookMyHome.Infrastructure/Repositories/BookingRepository.cs
@@ -1,44 +1,42 @@
 ﻿using BookMyHome.Application.RepositoryInterface;
 using BookMyHome.Domain.Entity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualBasic;
 
-namespace BookMyHome.Infrastructure.Repositories
+namespace BookMyHome.Infrastructure.Repositories;
+
+public class BookingRepository : IBookingRepository
 {
-    public class BookingRepository : IBookingRepository
+    private readonly BookMyHomeContext _db;
+
+    public BookingRepository(BookMyHomeContext context)
     {
-        private readonly BookMyHomeContext _db;
+        _db = context;
+    }
 
-        public BookingRepository(BookMyHomeContext context)
-        {
-            _db = context;
-        }
+    void IBookingRepository.AddBooking(Booking booking)
+    {
+        _db.Bookings.Add(booking);
+        _db.SaveChanges();
+    }
 
-        void IBookingRepository.AddBooking(Booking booking)
-        {
-            _db.Bookings.Add(booking);
-            _db.SaveChanges();
-        }
+    Booking IBookingRepository.GetBooking(int id)
+    {
+        return _db.Bookings
+            .Include(b => b.Accommodation)
+            .Single(b => b.Id == id);
+    }
 
-        Booking IBookingRepository.GetBooking(int id)
-        {
-            return _db.Bookings
-                .Include(b => b.Accommodation)
-                .Single(b => b.Id == id);
-        }
+    void IBookingRepository.UpdateBooking(Booking booking, byte[] rowversion)
+    {
+        _db.Entry(booking).Property(nameof(booking.RowVersion)).OriginalValue = rowversion;
+        _db.SaveChanges();
+    }
 
-        void IBookingRepository.UpdateBooking(Booking booking, byte[] rowversion)
-        {
-            _db.Entry(booking).Property(nameof(booking.RowVersion)).OriginalValue = rowversion;
-            _db.SaveChanges();
-        }
-
-        void IBookingRepository.DeleteBooking(Booking booking, byte[] rowversion)
-        {
-            // --- RowVersion? 
-            _db.Entry(booking).Property(nameof(booking.RowVersion)).OriginalValue = rowversion;
-            _db.Bookings.Remove(booking);
-            _db.SaveChanges();
-        }
+    void IBookingRepository.DeleteBooking(Booking booking, byte[] rowversion)
+    {
+        // --- RowVersion? 
+        _db.Entry(booking).Property(nameof(booking.RowVersion)).OriginalValue = rowversion;
+        _db.Bookings.Remove(booking);
+        _db.SaveChanges();
     }
 }
