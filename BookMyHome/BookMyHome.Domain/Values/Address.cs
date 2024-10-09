0056f64b3cbb7021cd339825d92e1aa0628fe1d0
Bilal Kinali <biki59986@edu.ucl.dk>
2024-10-09 20:34:32 +0200

    fixed nuget

--- BookMyHome/BookMyHome.Domain/Values/Address.cs
+++ BookMyHome/BookMyHome.Domain/Values/Address.cs
@@ -32,7 +32,6 @@ public record Address : ValueBase
         var dawaService = serviceProvider.GetRequiredService<IValidateAddressDomainService>();
         var dawaValidationRespose = dawaService.ValidateAddress(street, building, city);
         return new Address(street, building, zipCode, city, dawaValidationRespose);
-        return new();
     }
 }
 
